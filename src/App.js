91cb1ce5cf0c9c92d781e8a3a888ea14b7bd943a
Bhopal Singh <bhopalsinghsis@gmail.com>
2019-01-06 21:27:38 +0400

    Listing of React Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person.js';
 
 class App extends Component {
+
+  state = {
+    persons : [
+      {id:'111', name:"Max", age:18},
+      {id:'222', name:"Manu", age:22},
+      {id:'333', name:"Bhopal", age:25}
+    ],
+    showPersons : false
+  }
+
+
+
+  togglePersonHandler= () => {
+    const showPerson = this.state.showPersons;
+    this.setState({
+      showPersons : !showPerson
+    });
+  }
+
+  deletePersonHandler = (personIndex) => {
+    //const persons = this.state.persons;//DEEP COPY
+    const persons = this.state.persons.slice();//SHALLOW COPY
+    //const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons:persons});
+  }
+
+  onChangeHandler = (event, id) => {
+
+    const personIndex = this.state.persons.findIndex(person => {
+      return person.id === id;
+    });
+    console.log("PersonIndex:"+personIndex);
+
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+    person.name = event.target.value;
+    console.log("Fetched Person:", person);
+
+    const persons = this.state.persons.slice();
+    persons[personIndex] = person;
+
+    this.setState({
+      persons :  persons
+    });
+
+
+  }
   render() {
+    const style = {
+      backgroundColor : 'white',
+      font :'inherit',
+      border:'1px solid blue',
+      padding:'8px',
+      cursor:'pointer'
+    }
+
+    let person = null;
+    if(this.state.showPersons) {
+      person = (<div>
+                {
+          this.state.persons.map((person, index) => {
+            return <Person name={person.name} age={person.age} change={(event)=>this.onChangeHandler(event, person.id)} 
+            click = {()=>this.deletePersonHandler(index)} key={person.id}/>;
+          })
+        }
+        
+    </div>);
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>TEST App</h1>
+        <p>This is really Working!!!</p>
+        <button  style={style} onClick={this.togglePersonHandler}>Toggle Person</button>
+         {person}
       </div>
     );
   }

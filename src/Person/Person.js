524286cb09256192d63991f50c36ba41221e2aac
Bhopal Singh <bhopalsinghsis@gmail.com>
2019-01-15 20:54:01 +0400

    React ErrorBoundary to handle Error and exception handling

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,7 +2,10 @@ import React from 'react';
 import classes from './Person.css';
 
 const Person = (props) => {
-
+    const random = Math.random();
+    if(random > 0.7) {
+        throw new Error("Something went wrong...");
+    }
     return (
         <div className={classes.Person}>
             <p onClick={props.click}>I am {props.name} and I am {props.age} years old!!!!</p>
